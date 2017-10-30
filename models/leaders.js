6856140a81030c7de47b3b811c8c681334779596
Axel Bock <axel@bockig.com>
2017-10-31 00:03:37 +0100

    assignment 2

---
+++ models/leaders.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var leadersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leadersSchema);
+
+module.exports = Leaders;
