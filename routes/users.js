431f693ade006d6be23fa5deb74236b1c9118245
bock <axel.bock@dilax.com>
2017-11-19 14:57:22 +0100

    assignment 3 - task 3

--- routes/users.js
+++ routes/users.js
@@ -62,4 +62,14 @@ router.get('/logout', (req, res) => {
     }
 });
 
+router.get('/', authenticate.verifyUser, authenticate.verifyAdminUser, (req, res, next) => {
+    User.find({})
+        .then((users) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(users);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+});
+
 module.exports = router;
