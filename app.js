bf56c16d7a6396a1eff5df1175813f4ef683b9e8
Axel Bock <axel@bockig.com>
2017-11-23 20:26:45 +0100

    file upload :-o

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 var app = express();
 
@@ -62,6 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 /*    MONGOOSE     */
 /* * * * * * * * * */
